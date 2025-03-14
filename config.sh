6524df92fcb9bbcca3d77a538ea1b362a1678c51
Missionkiller2 <m244541maksim2@gmail.com>
2025-03-14 21:52:25 +0500

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pkg update
+pkg upgrate
+pkg install -y python git
+pip install --quiet telethon
+git clone https://github.com/Missionkiller2/userbot.git
+unzip userbot
+cd userbot
+unzip userbot
+cd userbot
+touch config.json 
+clear
+python main.py
+clear
+python bot.py
