82d0b0f3b333a7a7fa3173ef9a0123f5d960ad7c
Missionkiller2 <m244541maksim2@gmail.com>
2025-03-14 21:49:49 +0500

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+pkg update
+pkg upgrate
+pkg install -y python git
+pip install --quiet telethon
+cd /storage/emulated/0/Download/Telegram/scripts/test
+git clone https://github.com/Missionkiller2/userbot.git
+unzip userbot
+cd userbot
+unzip userbot
+cd userbot
+touch config.json 
+clear
+python main.py
+clear
+python bot.py
